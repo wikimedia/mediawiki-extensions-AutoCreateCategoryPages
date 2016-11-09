a03fde84061b9a1b66bf8283aa4cb15e6eeee2e6
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2016-11-09 22:57:11 +0200

    Minor code styling
    Change-Id: If35c713d6039b208a13ff0e7cd7c48742492ee17

--- AutoCreateCategoryPages.body.php
+++ AutoCreateCategoryPages.body.php
@@ -7,57 +7,67 @@ class AutoCreateCategoryPages {
 	 * @return array
 	 */
 	static function getExistingCategories() {
-		// TODO: cache this. Probably have to add to said cache every time a category page is created, by us or manually
-		$dbr = wfGetDB ( DB_SLAVE );
+		// TODO: cache this. Probably have to add to said cache every time a category page is created,
+		// by us or manually
+		$dbr = wfGetDB( DB_SLAVE );
 		$res = $dbr->select( 'page', 'page_title', array( 'page_namespace' => NS_CATEGORY ) );
-		
+
 		$categories = array();
-		foreach( $res as $row ) {
+		foreach ( $res as $row ) {
 			$categories[] = $row->page_title;
 		}
-		
+
 		return $categories;
 	}
 
 	/* after the page is saved, get all the categories
 	 * and see if they exist as "proper" pages; if not,
-	 * create a simple page for them automatically 
+	 * create a simple page for them automatically
 	 */
 
 	public static function onPageContentSaveComplete(
-		WikiPage $article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId
+		WikiPage $article, $user, $content, $summary, $isMinor,
+		$isWatch, $section, $flags, $revision, $status, $baseRevId
 	) {
 
 		global $wgAutoCreateCategoryStub;
 
 		// Extract the categories on this page
-		//$article->getParserOptions();
 		$page_cats = $article->getParserOutput( $article->makeParserOptions( $user ) )->getCategories();
 		// array keys will cast numeric category names to ints
 		// so we need to cast them back to strings to avoid potentially breaking things!
 		$page_cats = array_map( 'strval', array_keys( $page_cats ) );
 		$existing_cats = self::getExistingCategories();
-		
+
 		// Determine which categories on page do not exist
 		$new_cats = array_diff( $page_cats, $existing_cats );
 		
 		if( count( $new_cats ) > 0 ) {
+			/*
+			 * TODO probably need to use User::newSystemUser()
+			 * MW 1.27+ is supposed to use SessionManager, which requires some changes.
+			 * See https://www.mediawiki.org/wiki/Manual:SessionManager_and_AuthManager/Updating_tips
+			 */
+
 			// Create a user object for the editing user and add it to the database
 			// if it is not there already
-			$editor = User::newFromName( wfMessage( 'autocreatecategorypages-editor' )->inContentLanguage()->text() );
+			$editor = User::newFromName(
+				wfMessage( 'autocreatecategorypages-editor' )->inContentLanguage()->text()
+			);
 			if ( !$editor->isLoggedIn() ) {
 				$editor->addToDatabase();
 			}
-			
+
 			$summary = wfMessage( 'autocreatecategorypages-createdby' )->inContentLanguage()->text();
 
-			foreach( $new_cats as $cat ) {
-				$catTitle = Title::newFromDBkey ( $cat )->getText();
-				$stub = ( $wgAutoCreateCategoryStub != null ) ? 
-						$wgAutoCreateCategoryStub : wfMessage( 'autocreatecategorypages-stub', $catTitle )->inContentLanguage()->text();
+			foreach ( $new_cats as $cat ) {
+				$catTitle = Title::newFromDBkey( $cat )->getText();
+				$stub = ( $wgAutoCreateCategoryStub != null ) ?
+						$wgAutoCreateCategoryStub
+						: wfMessage( 'autocreatecategorypages-stub', $catTitle )->inContentLanguage()->text();
 
 				$safeTitle = Title::makeTitleSafe( NS_CATEGORY, $cat );
-				$catPage = new WikiPage( $safeTitle  );
+				$catPage = new WikiPage( $safeTitle );
 				try {
 					$content = ContentHandler::makeContent( $stub, $safeTitle );
 					$catPage->doEditContent( $content, $summary, EDIT_NEW & EDIT_SUPPRESS_RC, false, $editor );
